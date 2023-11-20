8160eaf164bd94d0b42901c998b078db68e13700
Leo Chan <leochan1110@gmail.com>
2023-11-20 16:59:53 +0800

    add sql

---
+++ sql/sql_script/test.sql
@@ -0,0 +1,15 @@
+WITH parse AS(
+    SELECT
+        group_concat(t2.keyword) as keyword,
+        t2.owner_id
+    FROM keyword_table t2
+    GROUP BY t2.owner_id
+)
+SELECT
+    id,
+    name,
+    description,
+    date_of_media,
+    keyword,
+    object_url
+FROM media_metadata JOIN parse ON parse.owner_id = media_metadata.id;
