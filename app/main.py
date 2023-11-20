9dced0b50f251a76a0fbded594abf046b2cafc21
Leo Chan <leochan1110@gmail.com>
2023-11-20 18:04:36 +0800

    add files

---
+++ app/main.py
@@ -0,0 +1,137 @@
+from fastapi import FastAPI, Request, Depends, status
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import uvicorn
+from blob_connector import BlobConnector
+# from database import MySQLConnector
+from pydantic import BaseModel
+from typing import Annotated
+from sql import models
+from sql.database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from sql.schemas import Media, Keyword
+from sql import models, schemas, crud
+import ast
+import json
+import re
+
+app = FastAPI(description="Sample FastAPI App")
+
+models.Base.metadata.create_all(bind=engine)
+
+
+template = Jinja2Templates(directory="template")
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.get("/", tags=["main"])
+async def root():
+	return {"message": "Hello World!!!"}
+	# return ["Hello", "World"]
+
+@app.get("/hello/{name}", tags=['main'])
+async def say_hello(name: int):
+	return {"message": f"Hello {name}"}
+
+@app.get("/file/{file_path:path}")
+async def read_file(file_path: str):
+	return {"file_path": file_path}
+
+item_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# params not part of path will be interpreted as query param
+# i.e. /items/?skip0&limit=10
+@app.get("/items/")
+async def read_item(q: str | None = None):
+	results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+	if q:
+		results.update({"q": q})
+	return results
+
+@app.post("/items/")
+async def create_item(item: Item):
+	item.name = item.name.capitalize()
+	item_dict = item.model_dump()
+	return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+	return {"item_id": item_id, **item.model_dump()}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+	item = {"item_id": item_id}
+	if q:
+		item.update({"q": q})
+	if not short:
+		item.update(
+			{"description": "This is an amazing item that has a long description"}
+		)
+	return item
+
+@app.get("/helloworld/{name}", response_class=HTMLResponse)
+async def say_hello(request: Request, name: str):
+	return template.TemplateResponse("index.html", {"request": request, "name": name})
+
+@app.get("/azure-blob")
+async def create_upload_file():
+	blob = BlobConnector()
+	blob.connect()
+	
+
+
+def get_db():
+	# mysql = MySQLConnector()
+	# mysql.connect()
+	db = SessionLocal()
+	try:
+		yield db
+		print("db activated")
+	finally:
+		db.close()
+		
+db_dependancy = Annotated[Session, Depends(get_db)]
+@app.post("/medias/", status_code=status.HTTP_201_CREATED)
+async def create_media(media: Media, db: db_dependancy):
+	db_media = models.Media(**media.model_dump())
+	# db_media.keyword = str(db_media.keyword)
+	print(db_media.name)
+	print(db_media.description)
+	print(db_media.date_of_media)
+	# print(db_media.keyword)
+	print(db_media.object_url)
+	db.add(db_media)
+	db.commit()
+
+
+@app.post("/media/{user_id}/keyword", status_code=status.HTTP_201_CREATED)
+async def create_media(keyword: Keyword, user_id: int, db: Session = Depends(get_db)):
+	keyword = crud.create_media_keyword(db, keyword=keyword, user_id=user_id)
+	return keyword
+
+@app.get("/media/{media_name}")
+def get_media_by_name(media_name: str, db: Session = Depends(get_db)):
+	keyword = crud.get_media_by_name(db, media_name=media_name)
+	return keyword
+
+
+@app.get("/media/get/")
+def get_media_by_keyword():
+	keyword_list = crud.get_media_by_keyword()
+	return keyword_list
+
+
+@app.get("/medias/")
+def read_medias():
+	medias = crud.get_media()
+	return medias
+
+
+if __name__ == '__main__':
+	uvicorn.run('main:app')
--- sql/schemas.py
+++ sql/schemas.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 from pydantic import BaseModel
+
+
+class Keyword(BaseModel):
+	keyword: str
+
+
 class Media(BaseModel):
 	name: str
 	description: str
 	date_of_media: datetime | None
-	keywords: list[str]
+	# keyword: list[Keyword] = []
 	object_url: str
 	
-	# def __init__(self):
 	
