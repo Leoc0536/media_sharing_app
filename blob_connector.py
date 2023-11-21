63939f55ca628354cb11e4817305b26682234288
Herman <ownherman126@gmail.com>
2023-11-21 09:38:24 +0800

    Ps

--- blob_connector.py
+++ blob_connector.py
@@ -9,7 +9,7 @@ class BlobConnector:
 		storage_account_name = os.getenv('storage_account_name')
 		connection_string = os.getenv('connection_string')
 		container_name = os.getenv('container_name')
-		
+
 		service = BlobServiceClient.from_connection_string(conn_str=connection_string)
 		# service.list_containers() returns iterable
 		for _ in service.list_containers():
@@ -18,15 +18,15 @@ class BlobConnector:
 		container_client = service.get_container_client(container_name)
 		for _ in container_client.list_blob_names():
 			print(_)
-		service.close()
 
-		# try:
-		# 	container_client = service.get_container_client(container=container_name)
-		# 	container_client.get_container_properties()
-		# except Exception as e:
-		# 	print(e)
-		# 	print("Creating container...")
-		# 	container_client = service.create_container(container_name)
+		try:
+			container_client = service.get_container_client(container=container_name)
+			container_client.get_container_properties()
+		except Exception as e:
+			print(e)
+			print("Creating container...")
+			container_client = service.create_container(container_name)
+		service.close()
 
 
 if __name__ == '__main__':
